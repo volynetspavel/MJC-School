2a36928cd2bf0b5c648d65d3030274e993817fed
Pavel Volynets <pavelsergeevich11@gmail.com>
2021-03-30 20:36:41 +0300

    add crud methods

--- Gift-Certificates/api/src/main/java/com/epam/esm/controller/TagController.java
+++ Gift-Certificates/api/src/main/java/com/epam/esm/controller/TagController.java
@@ -2,9 +2,8 @@ package com.epam.esm.controller;
 
 import com.epam.esm.model.Tag;
 import com.epam.esm.service.TagService;
-import com.epam.esm.service.impl.TagServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -12,15 +11,41 @@ import java.util.List;
 @RestController
 @RequestMapping("/tag")
 public class TagController {
-    private TagServiceImpl tagService;
+    private TagService tagService;
 
     @Autowired
-    public TagController(TagServiceImpl tagService) {
+    public TagController(TagService tagService) {
         this.tagService = tagService;
     }
 
+    @ResponseStatus(HttpStatus.OK)
     @GetMapping("/all")
-    public String findAll() {
-        return tagService.findAll().toString();
+    public List<Tag> findAll() {
+        return tagService.findAll();
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping("/{id}")
+    public Tag findById(@PathVariable int id) {
+        return tagService.findById(id);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable("id") int id) {
+        tagService.delete(id);
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @PutMapping("/{id}")
+    public void update(@PathVariable("id") int id, @RequestBody Tag tag) {
+        tag.setId(id);
+        tagService.update(tag);
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @PostMapping
+    public void insert(@RequestBody Tag tag) {
+        tagService.insert(tag);
     }
 }
