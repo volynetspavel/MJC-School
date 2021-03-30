53a702ee146af61c812092fc4c16a9db61ecb8e5
Pavel Volynets <pavelsergeevich11@gmail.com>
2021-03-30 09:12:08 +0300

    add configuration for connection via tomcat

---
+++ Gift-Certificates/api/src/main/java/com/epam/esm/config/WebConfig.java
@@ -0,0 +1,15 @@
+package com.epam.esm.config;
+
+import com.epam.esm.controller.TagController;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.epam.esm")
+public class WebConfig implements WebMvcConfigurer {
+
+}
