2774c75199a0a1aff38011b0615626dd1c145f1d
Pavel Volynets <pavelsergeevich11@gmail.com>
2021-04-01 17:36:27 +0300

    add constants for certificate table

---
+++ Gift-Certificates/core/src/main/java/com/epam/esm/constant/CertificateTableColumn.java
@@ -0,0 +1,14 @@
+package com.epam.esm.constant;
+
+/**
+ * Contains a list of the column names in the table.
+ */
+public class CertificateTableColumn {
+    public static final String ID = "id";
+    public static final String NAME = "name";
+    public static final String DESCRIPTION = "description";
+    public static final String PRICE = "price";
+    public static final String DURATION = "duration";
+    public static final String CREATE_DATE = "create_date";
+    public static final String LAST_UPDATE_DATE = "last_update_date";
+}
