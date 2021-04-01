31fc1d1920b96c199da79270e5555a42e9a7086b
Pavel Volynets <pavelsergeevich11@gmail.com>
2021-04-01 17:33:26 +0300

    add dto classes

---
+++ Gift-Certificates/core/src/main/java/com/epam/esm/dto/TagDto.java
@@ -0,0 +1,13 @@
+package com.epam.esm.dto;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * Class-wrapper for tag.
+ */
+@Data
+@EqualsAndHashCode(callSuper = false)
+public class TagDto extends AbstractDto{
+    private String name;
+}
