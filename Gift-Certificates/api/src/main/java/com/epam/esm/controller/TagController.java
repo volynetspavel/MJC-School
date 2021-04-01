ebf0c43f8310fbbb1c8c14999ea86c514b39a7d9
Pavel Volynets <pavelsergeevich11@gmail.com>
2021-04-01 17:38:01 +0300

    refactoring according dto

--- Gift-Certificates/api/src/main/java/com/epam/esm/controller/TagController.java
+++ Gift-Certificates/api/src/main/java/com/epam/esm/controller/TagController.java
@@ -1,6 +1,8 @@
 package com.epam.esm.controller;
 
-import com.epam.esm.model.Tag;
+import com.epam.esm.dto.TagDto;
+import com.epam.esm.exception.ResourceAlreadyExistException;
+import com.epam.esm.exception.ResourceNotFoundException;
 import com.epam.esm.service.TagService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -8,6 +10,9 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+/**
+ * Class is used to send requests from the client to the service layer for tag entity.
+ */
 @RestController
 @RequestMapping("/tag")
 public class TagController {
@@ -19,33 +24,34 @@ public class TagController {
     }
 
     @ResponseStatus(HttpStatus.OK)
+    @PostMapping
+    public void insert(@RequestBody TagDto tag) throws ResourceAlreadyExistException {
+        tagService.insert(tag);
+    }
+
+    @ResponseStatus(HttpStatus.OK)
     @GetMapping("/all")
-    public List<Tag> findAll() {
+    public List<TagDto> findAll() throws ResourceNotFoundException {
         return tagService.findAll();
     }
 
     @ResponseStatus(HttpStatus.OK)
     @GetMapping("/{id}")
-    public Tag findById(@PathVariable int id) {
+    public TagDto findById(@PathVariable int id) throws ResourceNotFoundException {
         return tagService.findById(id);
     }
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping("/{id}")
-    public void delete(@PathVariable("id") int id) {
+    public void delete(@PathVariable("id") int id) throws ResourceNotFoundException {
         tagService.delete(id);
     }
 
     @ResponseStatus(HttpStatus.OK)
     @PutMapping("/{id}")
-    public void update(@PathVariable("id") int id, @RequestBody Tag tag) {
+    public void update(@PathVariable("id") int id, @RequestBody TagDto tag)
+            throws ResourceNotFoundException, ResourceAlreadyExistException {
         tag.setId(id);
         tagService.update(tag);
-    }
-
-    @ResponseStatus(HttpStatus.OK)
-    @PostMapping
-    public void insert(@RequestBody Tag tag) {
-        tagService.insert(tag);
     }
 }
