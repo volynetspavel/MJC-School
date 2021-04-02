20d9a4edb24f17a0b9d17071e155a3bb2d8a8e5d
Pavel Volynets <pavelsergeevich11@gmail.com>
2021-04-02 11:06:57 +0300

    add NamedParameterJdbcTemplate for dao sql-requests

--- Gift-Certificates/core/src/test/java/com/epam/esm/config/TestDBConfig.java
+++ Gift-Certificates/core/src/test/java/com/epam/esm/config/TestDBConfig.java
@@ -1,15 +1,5 @@
 package com.epam.esm.config;
 
-import com.epam.esm.dao.AbstractDao;
-import com.epam.esm.dao.CertificateDao;
-import com.epam.esm.dao.db.CertificateDaoImpl;
-import com.epam.esm.dao.db.TagDaoImpl;
-import com.epam.esm.model.Certificate;
-import com.epam.esm.model.Tag;
-import com.epam.esm.service.CertificateService;
-import com.epam.esm.service.TagService;
-import com.epam.esm.service.impl.CertificateServiceImpl;
-import com.epam.esm.service.impl.TagServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -17,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 import javax.sql.DataSource;
@@ -44,4 +35,8 @@ public class TestDBConfig {
         return new JdbcTemplate(dataSource);
     }
 
+    @Bean
+    public NamedParameterJdbcTemplate namedParameterJdbcTemplate() {
+        return new NamedParameterJdbcTemplate(dataSource());
+    }
 }
