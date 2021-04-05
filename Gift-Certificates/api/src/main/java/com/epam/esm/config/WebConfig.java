8971bb3f32586c53419cd7f2444fa94174d3c5f6
Pavel Volynets <pavelsergeevich11@gmail.com>
2021-04-05 14:03:42 +0300

    refactoring

--- Gift-Certificates/api/src/main/java/com/epam/esm/config/WebConfig.java
+++ Gift-Certificates/api/src/main/java/com/epam/esm/config/WebConfig.java
@@ -1,7 +1,5 @@
 package com.epam.esm.config;
 
-import com.epam.esm.controller.TagController;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
