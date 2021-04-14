45f79b3630a5f25893fed2dc4366c14caf837a3d
Pavel Volynets <pavelsergeevich11@gmail.com>
2021-04-14 10:29:07 +0300

    part2: add entity user

--- Gift-Certificates(Advanced)/core/src/main/java/com/epam/esm/model/AbstractEntity.java
+++ Gift-Certificates(Advanced)/core/src/main/java/com/epam/esm/model/AbstractEntity.java
@@ -3,12 +3,19 @@ package com.epam.esm.model;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
 /**
  * Entity is a base class of all other entities.
  */
+@MappedSuperclass
 @Data
 @NoArgsConstructor
-public abstract class AbstractEntity {
-
-    private int id;
+public abstract class AbstractEntity<T extends Number> {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private T id;
 }
---
+++ Gift-Certificates(Advanced)/core/src/main/java/com/epam/esm/model/User.java
@@ -0,0 +1,23 @@
+package com.epam.esm.model;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+/**
+ * User is an entity of user.
+ */
+@Entity
+@Table(name = "user")
+@Data
+@NoArgsConstructor
+@EqualsAndHashCode(callSuper = false)
+public class User extends AbstractEntity<Integer> {
+
+    private String name;
+    private String surname;
+    private String email;
+}
